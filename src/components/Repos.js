aab20e81ec328784483d3b5ff294c4e09117efef
ferry-484 <80960981+ferry-484@users.noreply.github.com>
2021-06-28 19:00:55 +0530

    Add files via upload

---
+++ src/components/Repos.js
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
+const Repos = () => {
+  const { repos } = React.useContext(GithubContext);
+  console.log(repos);
+
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13",
+    },
+    {
+      label: "CSS",
+      value: "170",
+    },
+    {
+      label: "Javascript",
+      value: "80",
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {/* <h2>repos component</h2> */}
+        <ExampleChart data={chartData} />
+      </Wrapper>
+    </section>
+  );
+};
+
+//fusion_charts
+
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+
+  div {
+    width: 100% !important;
+  }
+  .fusioncharts-container {
+    width: 100% !important;
+  }
+  svg {
+    width: 100% !important;
+    border-radius: var(--radius) !important;
+  }
+`;
+
+export default Repos;
